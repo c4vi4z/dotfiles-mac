39e0e43373afc2021ae82fec8eeac3615236b087
caviaz <caviaz@pm.me>
2025-10-04 16:00:07 +0200

    remove comments apple.sh

--- sketchybar/items/apple.sh
+++ sketchybar/items/apple.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# --------------------------
-# APPLE MENU CLONE - 44pt BAR, SF SYMBOLS, FIXED CLICK_SCRIPTS
-# --------------------------
-
 BAR_ICON_SIZE=15.0
-ICON_COLOR=0xff7aa2f7   # adjust to match your bar color scheme
+ICON_COLOR=0xff7aa2f7 
 ICON_PADDING=12
 
-# Apple logo
 sketchybar --add item apple left
 sketchybar --set apple icon=􀣺 \
                        label.drawing=off \
@@ -17,7 +12,6 @@ sketchybar --set apple icon=􀣺 \
                        icon.padding_right=${ICON_PADDING} \
                        click_script='open -a "System Settings"'
 
-# Sleep
 sketchybar --add item apple_sleep left
 sketchybar --set apple_sleep icon=􀥦 \
                                  label.drawing=off \
@@ -26,7 +20,6 @@ sketchybar --set apple_sleep icon=􀥦 \
                                  icon.padding_right=${ICON_PADDING} \
                                  click_script='pmset sleepnow'
 
-# Restart
 sketchybar --add item apple_restart left
 sketchybar --set apple_restart icon=􀅈 \
                                    label.drawing=off \
@@ -35,7 +28,6 @@ sketchybar --set apple_restart icon=􀅈 \
                                    icon.padding_right=${ICON_PADDING} \
                                    click_script='osascript -e "tell application \"System Events\" to restart"'
 
-# Shutdown
 sketchybar --add item apple_shutdown left
 sketchybar --set apple_shutdown icon=􀆨 \
                                     label.drawing=off \
